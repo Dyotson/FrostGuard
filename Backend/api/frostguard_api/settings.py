1715267923386b6f1210354ff78e502d6965367f
Ignacio Munoz <itmunoz1@uc.cl>
2024-10-04 16:38:17 -0300

    add postgres config

--- Backend/api/frostguard_api/settings.py
+++ Backend/api/frostguard_api/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
   '192.168.1.35',
+  '127.0.0.1',
+  'localhost',
 ]
 
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frostguard_api',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +79,14 @@ WSGI_APPLICATION = 'frostguard_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "PORT": 5432,
+        "HOST": "postgres",
+    }
 }
 
 
