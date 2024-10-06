d60a5fea333b51a542733d2c4bcf2f6553759a25
diegocostares <dcosta@buk.cl>
2024-10-06 07:03:04 -0300

    fix: arreglo mapa

--- Frontend/app/dashboard/general/page.tsx
+++ Frontend/app/dashboard/general/page.tsx
@@ -22,14 +22,18 @@ import { GuardianPositionData, GuardianTelemetryData } from "@/lib/interfaces";
 export default function GeneralPage() {
   const [selectedZone, setSelectedZone] = useState<string>("Zona 1");
   const [zoneData, setZoneData] = useState<GuardianPositionData[]>([]);
-  const [telemetryData, setTelemetryData] = useState<GuardianTelemetryData[]>([]);
-  const [climateData, setClimateData] = useState<GuardianTelemetryData | null>(null);
+  const [telemetryData, setTelemetryData] = useState<GuardianTelemetryData[]>(
+    []
+  );
+  const [climateData, setClimateData] = useState<GuardianTelemetryData | null>(
+    null
+  );
 
   useEffect(() => {
     const fetchZoneData = async () => {
       const response = await fetchGuardianPositionDataByZone(selectedZone);
       if (response.length > 0) {
-        setZoneData(response);  // Set the entire response array
+        setZoneData(response);
       }
     };
 
@@ -47,7 +51,7 @@ export default function GeneralPage() {
 
   useEffect(() => {
     const fetchTelemetryData = async () => {
-      const response = await fetchAllTelemetryData();  // Fetch telemetry data for all guardians
+      const response = await fetchAllTelemetryData();
       setTelemetryData(response);
     };
 
@@ -60,7 +64,7 @@ export default function GeneralPage() {
     <>
       <div className="mb-6 flex justify-between items-center">
         <Select value={selectedZone} onValueChange={setSelectedZone}>
-          <SelectTrigger className="w-[180px]">
+          <SelectTrigger className="w-[180px] transition duration-200 ease-in-out hover:shadow-md">
             <SelectValue placeholder="Seleccionar zona" />
           </SelectTrigger>
           <SelectContent>
@@ -105,13 +109,15 @@ export default function GeneralPage() {
         <CardHeader>
           <CardTitle>Mapa del Terreno</CardTitle>
         </CardHeader>
-        <CardContent className="h-[600px]">
+        <CardContent>
           {zoneData.length > 0 ? (
-            <HomeMap
-              zoneData={zoneData}
-              telemetryData={telemetryData}  // Pass telemetry data to HomeMap
-              zoom={15}
-            />
+            <div className="w-full h-[500px] sm:h-[600px]">
+              <HomeMap
+                zoneData={zoneData}
+                telemetryData={telemetryData}
+                zoom={15}
+              />
+            </div>
           ) : (
             <p>No hay datos disponibles para esta zona</p>
           )}
--- Frontend/components/HomeMap.tsx
+++ Frontend/components/HomeMap.tsx
@@ -40,13 +40,23 @@ export default function HomeMap({
     return <div>Cargando mapa...</div>;
   }
 
-  const mapContainerStyle = {
-    width: "1350px",
-    height: "600px",
-  };
+  // Ordenamos las coordenadas del polígono
+  const sortCoordinatesClockwise = (
+    coordinates: { lat: number; lng: number }[]
+  ) => {
+    const centroid = coordinates.reduce(
+      (acc, point) => ({
+        lat: acc.lat + point.lat / coordinates.length,
+        lng: acc.lng + point.lng / coordinates.length,
+      }),
+      { lat: 0, lng: 0 }
+    );
 
-  const mapOptions = {
-    mapTypeId: "satellite",
+    return coordinates.sort((a, b) => {
+      const angleA = Math.atan2(a.lat - centroid.lat, a.lng - centroid.lng);
+      const angleB = Math.atan2(b.lat - centroid.lat, b.lng - centroid.lng);
+      return angleA - angleB;
+    });
   };
 
   // Encuentra los datos de telemetría más recientes de un sender
@@ -71,109 +81,88 @@ export default function HomeMap({
     });
   };
 
-  // Ordena las coordenadas usando el Algoritmo del Centroide (para evitar distorsión en los polígonos)
-  const sortCoordinatesClockwise = (
-    coordinates: { lat: number; lng: number }[]
-  ) => {
-    // Calcula el centroide
-    const centroid = coordinates.reduce(
-      (acc, point) => {
-        return {
-          lat: acc.lat + point.lat / coordinates.length,
-          lng: acc.lng + point.lng / coordinates.length,
-        };
-      },
-      { lat: 0, lng: 0 }
-    );
-
-    // Ordena las coordenadas en sentido horario
-    return coordinates.sort((a, b) => {
-      const angleA = Math.atan2(a.lat - centroid.lat, a.lng - centroid.lng);
-      const angleB = Math.atan2(b.lat - centroid.lat, b.lng - centroid.lng);
-      return angleA - angleB;
-    });
-  };
-
-  const roundValue = (value: number) => Math.round(value * 100) / 100;
-
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      center={{
-        lat: zoneData[0].latitude_i,
-        lng: zoneData[0].longitude_i,
-      }}
-      zoom={zoom}
-      options={mapOptions}
-    >
-      {zoneData.map((guardian) => (
-        <div key={guardian.id}>
-          <Polygon
-            paths={sortCoordinatesClockwise(guardian.guardian_zone.coordinates)}
-            options={{
-              fillColor: "#2196F3",
-              fillOpacity: 0.1,
-              strokeColor: "#1E88E5",
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-            }}
-          />
-          <Marker
-            position={{
-              lat: guardian.latitude_i,
-              lng: guardian.longitude_i,
-            }}
-            icon={{
-              url: "/assets/map.svg",
-              scaledSize: new google.maps.Size(30, 40),
-            }}
-            onClick={() => handleMarkerClick(guardian)}
-          />
-          <Circle
-            center={{
-              lat: guardian.latitude_i,
-              lng: guardian.longitude_i,
-            }}
-            radius={100}
-            options={{
-              fillColor: "#FF0000",
-              fillOpacity: 0.2,
-              strokeColor: "#FF0000",
-              strokeOpacity: 0.5,
-              strokeWeight: 1,
-            }}
-          />
-        </div>
-      ))}
-
-      {activeMarkerPosition && selectedTelemetry && (
-        <InfoWindow
-          position={activeMarkerPosition}
-          onCloseClick={() => setActiveMarkerPosition(null)}
-        >
-          <div className="px-2 pb-2 bg-white rounded shadow-md text-gray-800">
-            <h3 className="text-lg font-semibold mb-2">
-              Datos de Telemetría del Guardian
-            </h3>
-            <p className="mb-1">
-              <span className="font-semibold">Temperatura:</span>{" "}
-              {roundValue(selectedTelemetry.temperature)}°C
-            </p>
-            <p className="mb-1">
-              <span className="font-semibold">Humedad Relativa:</span>{" "}
-              {roundValue(selectedTelemetry.relative_humidity)}%
-            </p>
-            <p className="mb-1">
-              <span className="font-semibold">Presión Barométrica:</span>{" "}
-              {roundValue(selectedTelemetry.barometric_pressure)} hPa
-            </p>
-            <p className="mb-1">
-              <span className="font-semibold">Timestamp:</span>{" "}
-              {new Date(selectedTelemetry.timestamp).toLocaleString()}
-            </p>
+    <div className="w-full h-full">
+      <GoogleMap
+        mapContainerClassName="w-full h-full"
+        center={{
+          lat: zoneData[0].latitude_i,
+          lng: zoneData[0].longitude_i,
+        }}
+        zoom={zoom}
+        options={{ mapTypeId: "satellite" }}
+      >
+        {zoneData.map((guardian) => (
+          <div key={guardian.id}>
+            <Polygon
+              paths={sortCoordinatesClockwise(
+                guardian.guardian_zone.coordinates
+              )}
+              options={{
+                fillColor: "#2196F3",
+                fillOpacity: 0.1,
+                strokeColor: "#1E88E5",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+              }}
+            />
+            <Marker
+              position={{
+                lat: guardian.latitude_i,
+                lng: guardian.longitude_i,
+              }}
+              icon={{
+                url: "/assets/map.svg",
+                scaledSize: new google.maps.Size(30, 40),
+              }}
+              onClick={() => handleMarkerClick(guardian)}
+            />
+            <Circle
+              center={{
+                lat: guardian.latitude_i,
+                lng: guardian.longitude_i,
+              }}
+              radius={100}
+              options={{
+                fillColor: "#FF0000",
+                fillOpacity: 0.1,
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.3,
+                strokeWeight: 1,
+              }}
+            />
           </div>
-        </InfoWindow>
-      )}
-    </GoogleMap>
+        ))}
+
+        {activeMarkerPosition && selectedTelemetry && (
+          <InfoWindow
+            position={activeMarkerPosition}
+            onCloseClick={() => setActiveMarkerPosition(null)}
+          >
+            <div className="p-2 bg-white rounded shadow-md text-gray-800">
+              <h3 className="text-base font-semibold text-blue-600 mb-2">
+                Datos de Telemetría del Guardian
+              </h3>
+              <p className="mb-1">
+                <span className="font-semibold">Temperatura:</span>{" "}
+                {selectedTelemetry.temperature}°C
+              </p>
+              <p className="mb-1">
+                <span className="font-semibold">Humedad Relativa:</span>{" "}
+                {selectedTelemetry.relative_humidity}%
+              </p>
+              <p className="mb-1">
+                <span className="font-semibold">Presión Barométrica:</span>{" "}
+                {selectedTelemetry.barometric_pressure} hPa
+              </p>
+              <p className="text-sm text-gray-500">
+                <span className="font-semibold">Timestamp:</span>{" "}
+                {new Date(selectedTelemetry.timestamp).toLocaleString()}
+              </p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    </div>
   );
 }
